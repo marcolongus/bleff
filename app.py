dc7fa8b043901c1fd5988f03654c3364f0c2e1b0
marcolongus <benjaminmarcolongo@gmail.com>
2023-08-21 11:11:32 -0300

    views

--- app.py
+++ app.py
@@ -1,32 +1,50 @@
-from flask import Flask, request, jsonify, render_template, redirect, views, url_for
-import sqlite3
-from contextlib import closing
+from flask import Flask, request, jsonify, redirect, url_for
+from views import views 
+
+#request, jsonify, render_template, redirect, views, url_for
+#from contextlib import closing
+#import sqlite3
 
 app = Flask(__name__)
 app.debug = True
-
-def create_connection():
-    return sqlite3.connect("dictionary.db")
 
 app.register_blueprint(views, url_prefix="/")
 
 @app.route('/post_definition', methods=['POST'])
 def post_definition():
     definition = request.json.get('definition')
-    word = 'Mono' # This should be replaced with the word you want to insert
+    print("Definition:\n")
+    print(definition)
+    print("\n\n\n")
+    return redirect(url_for('views.lobby'))
 
-    with closing(create_connection()) as connection:
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO words VALUES (?, ?)", (word, definition))
-        connection.commit()
-        cursor.execute("SELECT * FROM words WHERE word=?", (word,))
-        print((cursor.fetchall()))
-		# Process the results if needed
-    return redirect('/definition_received')
-
-@app.route('/definition_received') # Corrected spelling here
-def definition_received():
-    return render_template('waitingHall.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
+
+
+
+
+
+
+
+
+
+# @app.route('/post_definition', methods=['POST'])
+# def post_definition():
+#     definition = request.json.get('definition')
+#     word = 'Mono' # This should be replaced with the word you want to insert
+
+#     with closing(create_connection()) as connection:
+#         cursor = connection.cursor()
+#         cursor.execute("INSERT INTO words VALUES (?, ?)", (word, definition))
+#         connection.commit()
+#         cursor.execute("SELECT * FROM words WHERE word=?", (word,))
+#         print((cursor.fetchall()))
+#         # Process the results if needed
+#     return redirect('/definition_received')
+
+# @app.route('/definition_received') # Corrected spelling here
+# def definition_received():
+#     return render_template('waitingHall.html')
--- views.py
+++ views.py
@@ -7,6 +7,6 @@ def home():
 	my_word = "libertario"
 	return render_template("main.html", word=my_word)
 
-@views.route("/")
-def waitingHall():
-	return render_template("waitingHall.html")
+@views.route("/lobby")
+def lobby():
+	return render_template("lobby.html")
